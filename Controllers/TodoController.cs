15c63237c9b69282a9e319970a713e856c7d4973
Saad Khan <165718548+Saadbarki@users.noreply.github.com>
2024-04-17 22:14:31 +0500

    Add files via upload

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using Todo.Entities;
+using Todo.Repositories;
+
+namespace Todo.Controllers
+{
+    [Route("api/TodoController")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoService todoService;
+        public TodoController(ITodoService todoService)
+        {
+            this.todoService = todoService;
+        }
+        [HttpGet("GetTodoList")]
+        public async Task<List<ImTodo>> GetTodoListAsync()
+        {
+            try
+            {
+                return await todoService.GetTodoListAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpGet("GetTodoByID")]
+        public async Task<IEnumerable<ImTodo>> GetTodoByIdAsync(int todoId)
+        {
+            try
+            {
+                var response =  await todoService.GetTodoListByIdAsync(todoId);
+                if (response == null)
+                {
+                    return null;
+                }
+                return response;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpPost("AddNewTodo")]
+        public async Task<IActionResult> AddNewTodoAsync(ImTodo todoObj)
+        {
+            if (todoObj == null)
+            {
+                return BadRequest(); ;
+            }
+            try
+            {
+
+                var response = await todoService.AddTodoAsync(todoObj);
+                return Ok(response);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpPut("UpdateTodo")]
+        public async Task<IActionResult> UpdateTodoAsync(ImTodo todoObj)
+        {
+            if (todoObj == null)
+            {
+                return BadRequest(); ;
+            }
+            try
+            {
+
+                var response = await todoService.UpdateTodoAsync(todoObj);
+                return Ok(response);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpDelete("DeleteTodo")]
+        public async Task<int> DeleteTodoAsync(int Id)
+        {
+            try
+            {
+
+                var response = await todoService.DeleteTodoAsync(Id);
+                return response;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Data/DbContextClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Data;
+using Todo.Entities;
+
+namespace Todo.Data
+{
+    public class DbContextClass:DbContext
+    {
+        public readonly IConfiguration Configuration;
+        public DbContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<ImTodo> TodoTab { get; set; }
+
+    }
+}
